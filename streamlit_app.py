0f88c0cdb2df009c84f05ca6d58160fb37777697
awsaruna <109497748+awsaruna@users.noreply.github.com>
2023-10-05 13:06:05 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import math
 import pandas as pd
 import streamlit as st
+import openai
 
 """
 # Welcome to Streamlit!
@@ -14,7 +15,23 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+# Setting the API key
+openai.api_key = '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'
 
+# Define the user prompt message
+prompt = "Hello!"
+# Create a chatbot using ChatCompletion.create() function
+completion = openai.ChatCompletion.create(
+  # Use GPT 3.5 as the LLM
+  model="gpt-3.5-turbo",
+  # Pre-define conversation messages for the possible roles
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": prompt}
+  ]
+)
+# Print the returned output from the LLM model
+print(completion.choices[0].message)
 
 with st.echo(code_location='below'):
     total_points = st.slider("This is Mr. Lucky", 1, 5000, 2000)
