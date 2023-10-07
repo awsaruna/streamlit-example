dc4bfad31df1823877eeb256cc21ea4876d4638d
Karthick Arun <karthickarun2009@gmail.com>
2023-10-07 15:23:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,8 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.image("https://e2009.s3.us-west-1.amazonaws.com/Untitled+Oct+04.png", width=200)
-st.title("TeenTalk")
-st.markdown("""#### Made by Karthick Arun""")
+st.markdown("""# TeenTalk""")
+st.markdown("""## Made by ### Karthick Arun""")
 st.divider()
 # Setting the API key
 openai.api_key = st.secrets["API_KEY"]
