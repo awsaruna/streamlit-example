780c58c16f2bf5b3df822d878a3beff095f7743f
Karthick Arun <karthickarun2009@gmail.com>
2023-10-07 10:33:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,21 @@ import math
 import pandas as pd
 import streamlit as st
 import openai
+from gtts import gTTS
+import base64
 
-"""
-# Teen Talk
-You life is very precious, you can make a difference in the world. I am here for you =)
-"""
+st.title("Teen Talk - Your life and happiness in your hands! :sunglasses:")
+
+st.divider()
+
 # Setting the API key
 openai.api_key = st.secrets["API_KEY"]
 
-user_input = st.text_input("What bothers you?", "say what bothers you")
-if st.button("Help!", type="primary"):
-    prompt = "Check the sentiment of the following phrase and if its negative then convert it to a positive phrase, start with a very supportive and understanding tone\n\nDesired format:\nOriginal Phrase:\nSentiment:\nConverted Phrase:\n\nText:'"+user_input+"'"
+user_input = st.text_input("You life is very precious, you can make a difference in the world. I am here for you....", "please type what your concerns here, I am here to help.")
+
+if st.button("Submit", type="primary"):
+    #prompt = "Check the sentiment of the following phrase and if its negative then convert it to a positive phrase, start with a very supportive and understanding tone\n\nDesired format:Original Phrase:,Sentiment:,Converted Phrase:\n\nText:'"+user_input+"'"
+    prompt = "You are a student councelor. Check the sentiment of the following phrase and if its negative then advise the student in an encouraging tone. Start with a very supportive and understanding tone. Just say the advise and no additional information\n\nText:'"+user_input+"'"
     # Create a chatbot using ChatCompletion.create() function
     completion = openai.ChatCompletion.create(
     # Use GPT 3.5 as the LLM
@@ -26,8 +30,12 @@ if st.button("Help!", type="primary"):
       ]
     )
     # Print the returned output from the LLM model
-    print(completion.choices[0].message)
-    st.write(completion.choices[0].message)
+   # print(completion.choices[0].message)
+    st.write(completion.choices[0].message["content"])
+
+    tts = gTTS(completion.choices[0].message["content"], lang="en", slow=False)
+    tts.save("output.mp3")
+    st.audio("output.mp3", format='audio/mp3')
     
 # Define the user prompt message
 #prompt = "Say the following sentence in a very positive and constructive way, start with a very supportive and understanding tone:'you look ugly and stupid'"
