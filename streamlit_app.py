4552191d6c163ce552f31b35ff573cf45334873c
Karthick Arun <karthickarun2009@gmail.com>
2023-10-07 13:45:23 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
+st.markdown("""
+<style>
+    body {
+        background-color: #fffdfa;
+    }
+    .stApp, .css-145kmo2, .css-17eq0hr, .css-qbe2hs, .css-1aumxhk, .css-17y0c9n, .css-1v3fh75, .css-t8sg0s, .css-1dbjc4n {
+        color: #8000ff !important;
+    }
+</style>
+""", unsafe_allow_html=True)
+
 from collections import namedtuple
 import altair as alt
 import math
@@ -7,16 +18,13 @@ import openai
 from gtts import gTTS
 import base64
 
-#add in branding above title
-
-st.title("Teen Talk - Your life and happiness in your hands! :sunglasses:")
+st.image("https://e2009.s3.us-west-1.amazonaws.com/Untitled+Oct+04.png", use_column_width=True)
+st.title("Teen Talk - Your life and happiness in your hands! :smile:")
 st.divider()
-
 # Setting the API key
 openai.api_key = st.secrets["API_KEY"]
 
-user_input = st.text_input("You life is very precious, you can make a difference in the world. I am here for you....", "please type what your concerns here, I am here to help.")
-
+user_input = st.text_input("Your life is very precious, you can make a difference in the world. I am here for you....", placeholder="please type what your concerns here, I am here to help.")
 if st.button("Submit", type="primary"):
     #prompt = "Check the sentiment of the following phrase and if its negative then convert it to a positive phrase, start with a very supportive and understanding tone\n\nDesired format:Original Phrase:,Sentiment:,Converted Phrase:\n\nText:'"+user_input+"'"
     prompt = "You are a student councelor. Check the sentiment of the following phrase and if its negative then advise the student in an encouraging tone. Start with a very supportive and understanding tone. Just say the advise and no additional information\n\nText:'"+user_input+"'"
